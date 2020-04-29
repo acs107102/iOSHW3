826b6a3d400700bc0b5e236bf4aaa0cbb254384c
“acs107102” <acs107102@gm.ntcu.edu.tw>
2020-04-29 10:22:48 +0800

    Initial Commit

---
+++ HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3
+//
+//  Created by Winnie on 2020/4/29.
+//  Copyright © 2020 Winnie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
